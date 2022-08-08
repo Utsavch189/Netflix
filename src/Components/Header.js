79d7b1a2e0b986309611fa20b1210c48f3f9f0a4
Utsav <utsavpokemon9000chatterjee@gmail.com>
2022-08-08 12:06:15 +0530

    hfdsguh

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,7 +23,7 @@ function Header() {
     <>
         <div className={scroll?"container-fluid header-main-scroll":"container-fluid header-main"}>
                 <img src="http://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png" alt="" style={{"height":"40px","width":"120px","backgroundColor":"transparent"}} className='mx-2'/>
-                <img src="https://tse4.mm.bing.net/th?id=OIP.M9sJUTCD2GNY_lmtj4iN4AHaHa&pid=Api&P=0" alt="" style={{"height":"30px","width":"40px"}}/>
+                <img src="https://tse4.mm.bing.net/th?id=OIP.M9sJUTCD2GNY_lmtj4iN4AHaHa&pid=Api&P=0" alt="" style={{"height":"30px","width":"40px","marginRight":"30px"}}/>
         </div>
     </>
   )
